11c52eb3bbcbfc5544cec42ebeb71f926cd1b988
Salma Ahmed <94996937+salmaahmed43@users.noreply.github.com>
2024-04-08 05:26:20 +0200

    Update main.py

--- main.py
+++ main.py
@@ -35,7 +35,7 @@ def predict_yolo(image):
     except Exception as e:
         return  str(e)
 
-@app.route('/predict', methods=['HEAD'])
+@app.route('/predict', methods=['POST','HEAD'])
 def predict():
     if 'image' not in request.files:
         return jsonify({'error': 'No image provided.'}), 400
