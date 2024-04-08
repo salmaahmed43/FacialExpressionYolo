442bd7d7a6eceff183ac44e734e893636832ac9e
Salma Ahmed <94996937+salmaahmed43@users.noreply.github.com>
2024-04-08 18:17:33 +0200

    Update main.py

--- main.py
+++ main.py
@@ -59,5 +59,5 @@ def predict():
 
             return jsonify(prediction_result), 200
 
-# if __name__ == '__main__':
-app.run(host='0.0.0.0',debug=True)
+if __name__ == '__main__':
+    app.run(host='0.0.0.0',debug=True)
