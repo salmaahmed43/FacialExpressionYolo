b466cdc98f1abf7eb6155035472885af8a098a02
Salma Ahmed <94996937+salmaahmed43@users.noreply.github.com>
2024-04-11 19:03:29 +0200

    Update main.py

--- main.py
+++ main.py
@@ -34,12 +34,9 @@ def predict_yolo(image):
         return class_names_string
     except Exception as e:
         return  str(e)
-@app.route('/')
-def hellow_word():
-    return "hellow word"
 
 
-@app.route('/predict', methods=['POST', 'GET'])
+@app.route('/predict', methods=['POST'])
 def predict():
     if request.method=='POST':
         if 'image' not in request.files:
